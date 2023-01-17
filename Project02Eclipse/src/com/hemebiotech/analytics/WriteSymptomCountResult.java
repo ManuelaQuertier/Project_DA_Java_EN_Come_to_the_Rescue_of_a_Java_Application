8d72b12cccdb7e26f75b000c8e9193fc9606b8df
Manuela QUERTIER <manuela.quertier@ext.biomerieux.com>
2023-01-17 11:45:24 +0100

    Clean the main Class, add WriteSymptom Class and SymptomsCounter Class

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomCountResult.java
@@ -0,0 +1,31 @@
+package com.hemebiotech.analytics;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteSymptomCountResult {
+
+	private String filepath;
+
+	public WriteSymptomCountResult(String filepath) {
+		this.filepath = filepath;
+	}
+
+	/**
+	 * 
+	 * @param filepath a full or partial path to file with symptom strings in it,
+	 *                 one per line
+	 */
+	public void writeSomething() {
+
+		if (filepath != null) {
+			try {
+				FileWriter writer = new FileWriter(filepath);
+				writer.write("hello");
+				writer.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
