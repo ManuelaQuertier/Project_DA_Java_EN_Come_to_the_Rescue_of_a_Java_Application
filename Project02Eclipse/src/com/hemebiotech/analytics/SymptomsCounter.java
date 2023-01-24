d65a45114a1a3d5014fe1761c2ea0057a31c26bf
Manuela QUERTIER <manuela.quertier@ext.biomerieux.com>
2023-01-24 15:51:34 +0100

    javadoc initialisation

--- Project02Eclipse/src/com/hemebiotech/analytics/SymptomsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/SymptomsCounter.java
@@ -5,7 +5,14 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class SymptomsCounter {
-
+	/**
+	 * This method order the symptoms in a treeMap and count the number of
+	 * occurrence for itch symptom
+	 * 
+	 * @param myList a list of symptoms
+	 * @return a treeMap where keys are the symptoms names and values are the number
+	 *         of occurrence
+	 */
 	public static Map<String, Integer> counter(List<String> myList) {
 
 		Map<String, Integer> myMap = new TreeMap<String, Integer>();
